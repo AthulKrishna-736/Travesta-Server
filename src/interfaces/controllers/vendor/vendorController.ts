e39118ac377880bcfd0042597ddfacfeed08cfb2
Athul krishna K S <athulkrishna736@gmail.com>
2025-05-14 11:26:51 +0530

    feat(vendor): vendor profile with s3 completed

--- src/interfaces/controllers/vendor/vendorController.ts
+++ src/interfaces/controllers/vendor/vendorController.ts
@@ -1,19 +1,34 @@
 import { Response } from "express";
-import { CreateUserDTO, UpdateUserDTO } from "../../dtos/user/user.dto";
-import { container, inject, injectable } from "tsyringe";
+import { UpdateUserDTO } from "../../dtos/user/user.dto";
+import { inject, injectable } from "tsyringe";
 import { AppError } from "../../../utils/appError";
 import { HttpStatusCode } from "../../../utils/HttpStatusCodes";
-import { env } from "../../../infrastructure/config/env";
-import { jwtConfig } from "../../../infrastructure/config/jwtConfig";
 import { ResponseHandler } from "../../../middlewares/responseHandler";
-import { IAuthService } from "../../../application/interfaces/authService.interface";
 import { TOKENS } from "../../../constants/token";
 import { CustomRequest } from "../../../utils/customRequest";
+import { IUpdateUserUseCase } from "../../../domain/interfaces/usecases.interface";
 
 @injectable()
 export class VendorController {
     constructor(
+        @inject(TOKENS.UpdateUserUseCase) private _updateUser: IUpdateUserUseCase,
     ) { }
 
+    async updateProfile(req: CustomRequest, res: Response): Promise<void> {
+        try {
+            const userId = req.user?.userId;
+            if (!userId) {
+                throw new AppError("User id is missing", HttpStatusCode.BAD_REQUEST);
+            }
 
+            const userData: UpdateUserDTO = req.body;
+
+            const updateUser = await this._updateUser.execute(userId, userData, req.file);
+
+            ResponseHandler.success(res, "Profile updated successfully", updateUser, HttpStatusCode.OK);
+        } catch (error: any) {
+            throw error;
+        }
+    }
 }
+
--- src/interfaces/routes/vendorRoutes.ts
+++ src/interfaces/routes/vendorRoutes.ts
@@ -1,41 +1,39 @@
 import { container } from "tsyringe";
 import { BaseRouter } from "./baseRouter";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { loginSchema, forgotPassSchema, updatePassSchema, verifyOtp, resendOtpSchema, createUserSchema, googleLoginSchema } from "../../shared/types/zodValidation";
+import { loginSchema, forgotPassSchema, updatePassSchema, verifyOtp, resendOtpSchema, createUserSchema, googleLoginSchema, updateUserSchema } from "../../shared/types/zodValidation";
 import { authMiddleware } from "../../middlewares/auth";
 import { CustomRequest } from "../../utils/customRequest";
 import { authorizeRoles } from "../../middlewares/roleMIddleware";
 import { AuthController } from "../controllers/base/authController";
 import { checkUserBlock } from "../../middlewares/checkBlock";
+import { upload } from "../../infrastructure/config/multer";
+import { VendorController } from "../controllers/vendor/vendorController";
 
 
 export class vendorRoutes extends BaseRouter {
-    private authController: AuthController
+    private _authController: AuthController
+    private _vendorController: VendorController
 
     constructor() {
         super();
-        this.authController = container.resolve(AuthController)
+        this._authController = container.resolve(AuthController)
+        this._vendorController = container.resolve(VendorController)
         this.initializeRoutes()
     }
 
     protected initializeRoutes(): void {
         this.router
-            .post('/auth/signup', validateRequest(createUserSchema), (req: CustomRequest, res) => this.authController.register(req, res))
-            .post('/auth/login', validateRequest(loginSchema), (req: CustomRequest, res) => this.authController.login(req, res))
-            .post('/auth/google-login', validateRequest(googleLoginSchema), (req: CustomRequest, res) => this.authController.loginGoogle(req, res))
-            .post('/auth/verifyOtp', validateRequest(verifyOtp), (req: CustomRequest, res) => this.authController.verifyOTP(req, res))
-            .post('/auth/resendOtp', validateRequest(resendOtpSchema), (req: CustomRequest, res) => this.authController.resendOtp(req, res))
-            .post('/auth/forgot-password', validateRequest(forgotPassSchema), (req: CustomRequest, res) => this.authController.forgotPassword(req, res))
-            .patch('/auth/reset-password', validateRequest(updatePassSchema), (req: CustomRequest, res) => this.authController.updatePassword(req, res))
-            .post('/auth/logout', authMiddleware, authorizeRoles('admin', 'vendor'), checkUserBlock, (req: CustomRequest, res) => this.authController.logout(req, res));
+            .post('/auth/signup', validateRequest(createUserSchema), (req: CustomRequest, res) => this._authController.register(req, res))
+            .post('/auth/login', validateRequest(loginSchema), (req: CustomRequest, res) => this._authController.login(req, res))
+            .post('/auth/google-login', validateRequest(googleLoginSchema), (req: CustomRequest, res) => this._authController.loginGoogle(req, res))
+            .post('/auth/verifyOtp', validateRequest(verifyOtp), (req: CustomRequest, res) => this._authController.verifyOTP(req, res))
+            .post('/auth/resendOtp', validateRequest(resendOtpSchema), (req: CustomRequest, res) => this._authController.resendOtp(req, res))
+            .post('/auth/forgot-password', validateRequest(forgotPassSchema), (req: CustomRequest, res) => this._authController.forgotPassword(req, res))
+            .patch('/auth/reset-password', validateRequest(updatePassSchema), (req: CustomRequest, res) => this._authController.updatePassword(req, res))
+            .post('/auth/logout', authMiddleware, authorizeRoles('admin', 'vendor'), checkUserBlock, (req: CustomRequest, res) => this._authController.logout(req, res))
 
 
-        this.router
-            .get('/', authMiddleware, (req, res) => {
-                res.send('Vendor authorized and authenticated successfully!');
-            });
-        // .put()  -> future: vendor profile update
-        // .patch() -> future: vendor update password
-        // .delete() -> future: vendor delete account
+            .patch('/profile', authMiddleware, authorizeRoles('admin', 'vendor'), checkUserBlock, upload.single('image'), validateRequest(updateUserSchema), (req: CustomRequest, res) => this._vendorController.updateProfile(req, res))
     }
 }
