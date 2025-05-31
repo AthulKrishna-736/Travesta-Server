9f4ed5b76ab1d4a017ed4e0455965cc61a6e4faa
Athul krishna K S <athulkrishna736@gmail.com>
2025-06-01 00:16:58 +0530

    feat: hotel entity implemented adding business logics

--- src/domain/entities/hotel.entity.ts
+++ src/domain/entities/hotel.entity.ts
@@ -1,5 +1,7 @@
 import { Types } from "mongoose"
-import { IHotel } from "../interfaces/model/hotel.interface"
+import { IHotel, TUpdateHotelData } from "../interfaces/model/hotel.interface"
+import { AppError } from "../../utils/appError"
+import { HttpStatusCode } from "../../utils/HttpStatusCodes"
 
 
 export interface IHotelEntity {
@@ -21,6 +23,11 @@ export interface IHotelEntity {
     readonly updatedAt: Date
 
 
+    //business logic
+    block(): void
+    unblock(): void
+    update(data: TUpdateHotelData): void
+
 
 }
 
@@ -31,69 +38,101 @@ export class HotelEntity implements IHotelEntity {
         this._props = data
     }
 
-    get id(){
+    get id() {
         return this._props._id
     }
 
-    get vendorId(){
+    get vendorId() {
         return this._props.vendorId
     }
 
-    get name(){
+    get name() {
         return this._props.name
     }
 
-    get description(){
+    get description() {
         return this._props.description
     }
 
-    get images(){
+    get images() {
         return this._props.images
     }
 
-    get rating(){
+    get rating() {
         return this._props.rating
     }
 
-    get services(){
+    get services() {
         return this._props.services
     }
 
-    get amenities(){
+    get amenities() {
         return this._props.amenities
     }
 
-    get tags(){
+    get tags() {
         return this._props.tags
     }
 
-    get state(){
+    get state() {
         return this._props.state
     }
 
-    get city(){
+    get city() {
         return this._props.city
     }
 
-    get address(){
+    get address() {
         return this._props.address
     }
 
-    get geoLocation(){
+    get geoLocation() {
         return this._props.geoLocation
     }
 
-    get isBlocked(){
+    get isBlocked() {
         return this._props.isBlocked
     }
 
-    get createdAt(){
+    get createdAt() {
         return this._props.createdAt
     }
 
-    get updatedAt(){
+    get updatedAt() {
         return this._props.updatedAt
     }
+
+    //business logic
+    block(): void {
+        if (this._props.isBlocked) {
+            throw new AppError('Hotel is already blocked', HttpStatusCode.CONFLICT)
+        }
+        this._props.isBlocked = false;
+        this._props.updatedAt = new Date();
+    }
+
+    unblock(): void {
+        if (!this._props.isBlocked) {
+            throw new AppError('Hotel is already unblocked', HttpStatusCode.CONFLICT)
+        }
+    }
+
+    update(data: TUpdateHotelData): void {
+        if (data.name) this._props.name = data.name
+        if (data.description) this._props.description = data.description
+        if (data.images) this._props.images = data.images
+        if (data.rating) this._props.rating = data.rating
+        if (data.services) this._props.services = data.services
+        if (data.amenities) this._props.amenities = data.amenities
+        if (data.tags) this._props.tags = data.tags
+        if (data.state) this._props.state = data.state
+        if (data.city) this._props.city = data.city
+        if (data.address) this._props.address = data.address
+        if (data.geoLocation) this._props.geoLocation = data.geoLocation
+        this._props.updatedAt = new Date()
+    }
+
+    
 
 
 }
