11ed385d43fc9780a80f328f4c0b89983d1eee09
Athul krishna K S <athulkrishna736@gmail.com>
2025-06-17 22:52:54 +0530

    feat(amenities): implemented amenities controller

--- src/interfaceAdapters/controllers/amenityController.ts
+++ src/interfaceAdapters/controllers/amenityController.ts
@@ -3,6 +3,8 @@ import { TOKENS } from "../../constants/token";
 import { IBlockUnblockAmenityUseCase, ICreateAmenityUseCase, IGetAllAmenitiesUseCase, IGetAmenityByIdUseCase, IUpdateAmenityUseCase } from "../../domain/interfaces/model/amenities.interface";
 import { CustomRequest } from "../../utils/customRequest";
 import { Response } from "express";
+import { ResponseHandler } from "../../middlewares/responseHandler";
+import { HttpStatusCode } from "../../utils/HttpStatusCodes";
 
 
 @injectable()
@@ -17,41 +19,51 @@ export class AmenityController {
 
     async createAmenity(req: CustomRequest, res: Response): Promise<void> {
         try {
-            
+            const data = req.body;
+            const amenity = await this._createAmenity.createAmenity(data);
+            ResponseHandler.success(res, "Amenity created", amenity, HttpStatusCode.CREATED);
         } catch (error) {
-
+            throw error;
         }
     }
 
     async updateAmenity(req: CustomRequest, res: Response): Promise<void> {
         try {
-
+            const id = req.params.id;
+            const data = req.body;
+            const result = await this._updateAmenity.updateAmenity(id, data);
+            ResponseHandler.success(res, result.message, result.amenity, HttpStatusCode.OK)
         } catch (error) {
-
+            throw error;
         }
     }
 
     async blockUnblockAmenity(req: CustomRequest, res: Response): Promise<void> {
         try {
-
+            const id = req.params.id;
+            const result = await this._blockUnblockAmenity.blockUnblockAmenityUseCase(id);
+            ResponseHandler.success(res, result.message, result.amenity, HttpStatusCode.OK)
         } catch (error) {
-
+            throw error;
         }
     }
 
     async getAmenityById(req: CustomRequest, res: Response): Promise<void> {
         try {
-
+            const id = req.params.id;
+            const result = await this._getAmenityById.getAmenityById(id);
+            ResponseHandler.success(res, result.message, result.amenity, HttpStatusCode.OK)
         } catch (error) {
-
+            throw error;
         }
     }
 
     async getAllAmenities(req: CustomRequest, res: Response): Promise<void> {
         try {
-
+            const result = await this._getAllAmenities.getAllAmenitiesUseCase();
+            ResponseHandler.success(res, result.message, result.amenities, HttpStatusCode.OK);
         } catch (error) {
-
+            throw error;
         }
     }
 }
