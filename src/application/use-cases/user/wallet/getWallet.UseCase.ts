a326dbe286dfd067f0a4600ab343287a84606421
Athul krishna K S <athulkrishna736@gmail.com>
2025-10-03 00:52:02 +0530

    refact: get wallet refactored & added admin wallet support

--- src/application/use-cases/user/wallet/getWallet.UseCase.ts
+++ src/application/use-cases/user/wallet/getWallet.UseCase.ts
@@ -1,29 +1,27 @@
-import { inject, injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe'
 import { TOKENS } from '../../../../constants/token';
 import { IWalletRepository } from '../../../../domain/interfaces/repositories/walletRepo.interface';
-import { IGetWalletUseCase } from '../../../../domain/interfaces/model/wallet.interface';
-import { AppError } from '../../../../utils/appError';
-import { HttpStatusCode } from '../../../../constants/HttpStatusCodes';
+import { ICreateWalletUseCase, IGetWalletUseCase } from '../../../../domain/interfaces/model/wallet.interface';
 import { WALLET_RES_MESSAGES } from '../../../../constants/resMessages';
-import { WALLET_ERROR_MESSAGES } from '../../../../constants/errorMessages';
 import { TResponseWalletDTO } from '../../../../interfaceAdapters/dtos/wallet.dto';
 import { ResponseMapper } from '../../../../utils/responseMapper';
 
 @injectable()
 export class GetWalletUseCase implements IGetWalletUseCase {
     constructor(
-        @inject(TOKENS.WalletRepository) private _walletRepository: IWalletRepository
+        @inject(TOKENS.WalletRepository) private _walletRepository: IWalletRepository,
+        @inject(TOKENS.CreateWalletUseCase) private _createWalletUseCase: ICreateWalletUseCase,
     ) { }
 
     async getUserWallet(userId: string): Promise<{ wallet: TResponseWalletDTO | null, message: string }> {
-        const wallet = await this._walletRepository.findUserWallet(userId);
+        const walletDoc = await this._walletRepository.findUserWallet(userId);
 
-        if (!wallet) {
-            throw new AppError(WALLET_ERROR_MESSAGES.notFound, HttpStatusCode.NOT_FOUND);
+        if (!walletDoc) {
+            const { wallet } = await this._createWalletUseCase.createUserWallet(userId);
+            return { wallet: wallet, message: WALLET_RES_MESSAGES.getWallet };
         }
 
-        const mappedWallet = ResponseMapper.mapWalletToResponseDTO(wallet);
-
+        const mappedWallet = ResponseMapper.mapWalletToResponseDTO(walletDoc);
         return { wallet: mappedWallet, message: WALLET_RES_MESSAGES.getWallet };
     }
 }
