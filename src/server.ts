d807f0f00fee909b4831047bf8747d47a93587e9
Athul krishna K S <athulkrishna736@gmail.com>
2025-12-27 01:16:00 +0530

    chore: container included in server file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,21 @@
 import 'reflect-metadata';
-import './infrastructure/config/container';
+import './infrastructure/config/containers/adminUseCaseContainer';
+import './infrastructure/config/containers/amenitiesUseCaseContainer';
+import './infrastructure/config/containers/authUseCaseContainer';
+import './infrastructure/config/containers/bookingUseCaseContainer';
+import './infrastructure/config/containers/chatUseCaseContainer';
+import './infrastructure/config/containers/controllersContainer';
+import './infrastructure/config/containers/couponUseCaseContainer';
+import './infrastructure/config/containers/hotelUseCaseContainer';
+import './infrastructure/config/containers/notificationUseCaseContainer';
+import './infrastructure/config/containers/offerUseCaseContainer';
+import './infrastructure/config/containers/ratingUseCaseContainer';
+import './infrastructure/config/containers/repositoriesContainer';
+import './infrastructure/config/containers/roomUseCaseContainer';
+import './infrastructure/config/containers/servicesContainer';
+import './infrastructure/config/containers/subscriptionUseCaseContainer';
+import './infrastructure/config/containers/walletUseCaseContainer';
+
 import { App } from './app';
 import { env } from './infrastructure/config/env';
 import { connectDB } from './infrastructure/config/db';
